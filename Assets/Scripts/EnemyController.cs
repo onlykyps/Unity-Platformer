da38fa0bf2853673d07188ce99d36edcf9fdd0c0
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-09-01 17:22:22 +0300

    enemy-controller
    setup enemy to move in a pattern

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 offsetEndPos;
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+    void Awake()
+    {
+        startPos = transform.position;
+        targetPos = startPos + offsetEndPos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        if(transform.position == targetPos)
+        {
+            if(targetPos == startPos)
+                targetPos = startPos + offsetEndPos;
+            else
+                targetPos = startPos;
+        }
+    }
+}
