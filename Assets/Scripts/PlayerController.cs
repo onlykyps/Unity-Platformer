99fd4cffb1704902af00771d4b5e1f6754e09b8a
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-09-01 16:01:25 +0300

    rotating the player
    to face the direction in which it is moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,9 +30,24 @@ public class PlayerController : MonoBehaviour
         // getting our inputs from keyboard
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
-
+        
+        // calculate the direction we need to move in
         Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
         dir.y = rig.velocity.y;
+
+        // set the rigidbody velocity
         rig.velocity = dir;
+
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+
+        // magnitude is the length of a vector
+        // when player is stationary facingDir becomes 0,0,0 as Move() is called repeatedly
+        // prevents last movement value from changing when player is stationary
+        if(facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
+        
+    
     }
 }
