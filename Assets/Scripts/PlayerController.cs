59156092a6998229af2ea7a97144e88b67075d94
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-09-01 15:46:12 +0300

    player movement
    unity and vs code plugins adjustments

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        // get the rigidbody component attatched to this object
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        // getting our inputs from keyboard
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rig.velocity.y;
+        rig.velocity = dir;
+    }
+}
